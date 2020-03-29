6f5af2148d6bc40ae015c5ace33568f5207b3878
ikirpichny <ikirpichny@mail.ru>
2020-03-29 15:23:56 +0300

    Lambdas02Exercise - done

--- lambda/src/test/java/lambda/part1/exercise/Lambdas02Exercise.java
+++ lambda/src/test/java/lambda/part1/exercise/Lambdas02Exercise.java
@@ -21,15 +21,21 @@ public class Lambdas02Exercise {
     @Test
     public void generic0() {
         //TODO Use anonymous class
-        final GenericProduct<Integer> prod = null;
-
+        final GenericProduct<Integer> prod = new GenericProduct<Integer>() {
+            @Override
+            public Integer prod(Integer a, int i) {
+                return a * i;
+            }
+        };
         assertEquals(prod.prod(3, 2), Integer.valueOf(6));
     }
 
     @Test
     public void generic1() {
         //TODO Use statement lambda
-        final GenericProduct<Integer> prod = null;
+        final GenericProduct<Integer> prod = (a, i) -> {
+            return a * i;
+        };
 
         assertEquals(prod.prod(3, 2), Integer.valueOf(6));
     }
@@ -37,25 +43,30 @@ public class Lambdas02Exercise {
     @Test
     public void generic2() {
         //TODO Use expression lambda
-        final GenericProduct<Integer> prod = null;
+        final GenericProduct<Integer> prod = (a, i) -> a * i;
 
         assertEquals(prod.prod(3, 2), Integer.valueOf(6));
     }
 
     private static String stringProd(String s, int i) {
         //TODO any realization (String, StringBuilder)
-        return "?";
+        StringBuilder sb = new StringBuilder();
+        for (int j = 1; j <= i; j++) {
+            sb.append(s);
+        }
+        return String.valueOf(sb);
     }
 
     private static String stringGenerator(String s, int i) {
         //TODO Use stream.generate
-        return "?";
+
+        return Stream.generate(() -> s).limit(i).collect(Collectors.joining());
     }
 
     @Test
     public void strSum() {
-        final GenericProduct<String> prod = null;
-        final GenericProduct<String> prod2 = null;
+        final GenericProduct<String> prod = (s, i) -> stringProd(s, i);
+        final GenericProduct<String> prod2 = Lambdas02Exercise::stringGenerator;
 
         assertEquals(prod.prod("a", 2), "aa");
         assertEquals(prod2.prod("a", 3), "aaa");
@@ -65,12 +76,16 @@ public class Lambdas02Exercise {
 
     private String stringSumWithDelimeter(String s, int i) {
         //TODO any realization (String, StringBuilder)
-        return "?";
+        StringBuilder sb = new StringBuilder(s);
+        for (int j = 2; j <= i; j++) {
+            sb.append(delimeter + s);
+        }
+        return String.valueOf(sb);
     }
 
     @Test
     public void strSum2() {
-        final GenericProduct<String> prod = null; // use stringSumWithDelimeter;
+        final GenericProduct<String> prod = (s, i) -> stringSumWithDelimeter(s, i); // use stringSumWithDelimeter;
 
         assertEquals(prod.prod("a", 3), "a-a-a");
     }
